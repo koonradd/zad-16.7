7ce3d0b7b967401d15b8bcea4b99463f5373b0dd
Konrad Kościelski <konrad.koscielski@interia.pl>
2018-04-11 23:26:22 +0200

    init

---
+++ src/components/Todo.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import style from './Todo.css'; 
+
+class Todo extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <li className={style.Todo}>
+                {this.props.todo} 
+                <button onClick={event => this.buttonClick()}>Remove</button>
+            </li>
+        )
+    }
+
+    buttonClick(event) {
+        this.props.remove(this.props.id);
+    }
+}
+
+export default Todo;import React from 'react';
+import style from './Todo.css'; 
+
+class Todo extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <li className={style.Todo}>
+                {this.props.todo} 
+                <button onClick={event => this.buttonClick()}>Remove</button>
+            </li>
+        )
+    }
+
+    buttonClick(event) {
+        this.props.remove(this.props.id);
+    }
+}
+
+export default Todo;
